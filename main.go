6cb33a465331efcfca254ef0c25f027de52e69d7
jakethecoder95 <95jacob07@gmail.com>
2024-12-10 21:24:52 -0800

    Complete day10

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	// "advent2024/day6"
-	// "advent2024/day7"
-	"advent2024/day9"
+	"advent2024/day10"
 	"fmt"
 )
 
@@ -14,6 +12,8 @@ func main() {
     // fmt.Println("Day 7, Part 2", day7.Part2())
     // fmt.Println("Day 8, Part 1", day8.Part1())
     // fmt.Println("Day 8, Part 2", day8.Part2())
-    fmt.Println("Day 9, Part 1", day9.Part1())
-    fmt.Println("Day 9, Part 2", day9.Part2())
+    // fmt.Println("Day 9, Part 1", day9.Part1())
+    // fmt.Println("Day 9, Part 2", day9.Part2())
+    fmt.Println("Day 10, Part 1", day10.Part1())
+    fmt.Println("Day 10, Part 2", day10.Part2())
 }
